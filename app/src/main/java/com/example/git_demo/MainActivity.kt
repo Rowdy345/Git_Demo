7b94f82d64a8a41f55af5981cf326f06c3b11ae8
smallbugg <pawan221magic@gmail.com>
2026-07-20 00:14:14 +0530

    commit main commit

--- app/src/main/java/com/example/git_demo/MainActivity.kt
+++ app/src/main/java/com/example/git_demo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Git_DemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android code",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }

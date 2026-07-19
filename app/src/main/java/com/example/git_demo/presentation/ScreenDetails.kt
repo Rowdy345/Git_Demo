37128b03297fa46e699da802d7f57ef89daa5133
Rowdy345 <pawan123magic@gmail.com>
2026-07-19 21:53:45 +0530

    My First Commit

---
+++ app/src/main/java/com/example/git_demo/presentation/ScreenDetails.kt
@@ -0,0 +1,16 @@
+package com.example.git_demo.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun ScreenDetails() {
+    Column(Modifier.fillMaxSize(), Arrangement.Center) {
+        Text("ScreenDetails")
+    }
+}
